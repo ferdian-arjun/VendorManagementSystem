6ec5a9050fb322d1cf3fdce1bbbad1a3a837a901
Ferdian Arjun <ferdianarjun.dev@gmail.com>
2024-01-16 14:46:48 +0700

    feat: validation form

--- VendorManagementSystem/ViewModel/RoleViewModel.cs
+++ VendorManagementSystem/ViewModel/RoleViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +10,9 @@ namespace VendorManagementSystem.ViewModel
     public class RoleViewModel
     {
         public string Guid { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50, ErrorMessage = "Name cannot exceed 50 characters")]
         public string Name {get; set; }
     }
 }
